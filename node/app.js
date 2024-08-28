9fb178d52bcd1342df1b8792c267f68c6237eaf7
nami <nami@cmu.edu>
2024-08-28 17:22:44 -0400

    just fixing node debugging

--- node/app.js
+++ node/app.js
@@ -1,5 +1,6 @@
 const http = require('http');
 const express = require('express');
+const path = require('path');
 const port = process.env.PORT || 3030;
 var app = express();
 
@@ -7,7 +8,8 @@ app.use(function (req, res, next) {
     console.log(req.url);
     next();
 });
-app.use(express.static('../'))
+
+app.use(express.static(path.join(__dirname, '../')))
 const server = http.createServer(app);
 console.log("\n\n Debug.. Listening on port: " + port);
 server.listen(port)
