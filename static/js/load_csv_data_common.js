83e727f0aaff84cc0629c9fc85f5873d1260b4a9
nami <nami@cmu.edu>
2024-09-15 21:49:27 -0400

    PROLIFIC_PID must be in the query param

--- static/js/load_csv_data_common.js
+++ static/js/load_csv_data_common.js
@@ -116,9 +116,8 @@ const getRandomTreatmentGroup = function(reviewSubjectElementsJson){
 }
 
 const submitSurvey = function() {
-    // the query parameter "UID" must be found in the url query string, as passed on from the feeding platform
-    // UID is the user ID
-    const UNIQUE_USER_KEY = "PID";
+    // the query parameter "PROLIFIC_PID" must be found in the url query string, as passed on from the feeding platform
+    const UNIQUE_USER_KEY = "PROLIFIC_PID";
     
     let urlParams = (new URL(window.location)).searchParams;
     var userID = urlParams.get(UNIQUE_USER_KEY);
